70501a51d8f4b38edd228e46032cb8ff1cf9d6cc
Masuk <masukrahoman5@gmail.com>
2026-04-19 18:03:51 +0600

    chore: config the auth file

---
+++ app/auth.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from jose import JWTError,jwt
+from passlib.context import CryptoContext
+from datetime import datetime,timedelta
+from typing import Optional
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM","HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",30))
+
+pwd_context = CryptoContext(schemes=["bcrypt"], deprecated='auto')
+
+def hash_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_pass:str,hashed_pass:str)->bool:
+    return pwd_context.verify(plain_pass,hashed_pass)
+
+def create_access_token(data:dict, expire_delta:Optional[timedelta]=None)->str:
+
+    to_encode = data.copy
+
+    if expire_delta:
+        expire = datetime.utcnow() + expire_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp":expire})
+
+    token = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return token
+
+def decode_token(token:str)-> Optional[str]:
+    try:
+        payload = jw.decode(token,SECRET_KEY,algorithm=ALGORITHM)
+        email : str = payload.get("sub")
+        return email
+    except JWTError:
+        return None
+    
+    
+
